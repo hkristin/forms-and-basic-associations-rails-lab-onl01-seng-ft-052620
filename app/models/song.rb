9716e6971fe4961d32605d37e9b8dc4fbd5f63dc
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-08 18:24:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,7 +15,19 @@ class Song < ActiveRecord::Base
   self.genre = Genre.find_or_create_by(name: name)
 end
 
-def genre_name
-  self.genre ? self.genre.name : nil
-end
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(contents)
+   contents.each do |content|
+     if content.strip != ""
+        self.notes.build(content: content)
+     end
+   end
+ end
+
+  def note_contents
+   self.notes.map(&:content)
+  end
 end
